3fdae8020f3c0c7a47a2b0babb52e91defdbb062
juhoi.kim <juhoi.kim@gmail.com>
2020-05-21 21:50:13 +0900

    ~ 2-04 조회와 템플릿

--- board/admin.py
+++ board/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Answer
 
 
 # Register your models here.
+# class AnswerInline(admin.StackedInline):
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 3
+
+
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('id', 'subject', 'content', 'create_date')
     search_fields = ['subject']
+    inlines = [AnswerInline]
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'content', 'create_date')
+    search_fields = ['question']
--- board/views.py
+++ board/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi~")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'board/question_list.html', context)
+    # return HttpResponse("Hi~")
+
+
+def detail(request, question_id):
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'board/question_detail.html', context)
